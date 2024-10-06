71a04150c34b490f67e2a3439b7ff099570dd7eb
SohalSawardekar <sohalsawardekar11@gmail.com>
2024-10-06 16:07:58 +0530

    booking backend

--- lib/screens/bookings/bookingConfirmation.dart
+++ lib/screens/bookings/bookingConfirmation.dart
@@ -29,17 +29,14 @@ class _BookingConfirmationPageState extends State<BookingConfirmationPage> {
 
   Future<void> addBookingToFirestore() async {
     try {
-      // Get the current user's ID
       User? user = FirebaseAuth.instance.currentUser;
 
       if (user != null) {
         String userId = user.uid;
 
-        // Reference to the 'Rooms' collection
         CollectionReference rooms =
             FirebaseFirestore.instance.collection('Rooms');
 
-        // Update the selected room's status to 'no' using the roomId
         await rooms.doc(widget.roomId).update({
           'status': 'no',
           'user-id': userId,
@@ -47,20 +44,17 @@ class _BookingConfirmationPageState extends State<BookingConfirmationPage> {
           'check-out': widget.checkOutDate,
         });
 
-        // Reference to the 'Bookings' collection
         CollectionReference bookings =
             FirebaseFirestore.instance.collection('Bookings');
 
-        // Data to add
         await bookings.add({
-          'userId': userId, // Adding the current user's ID
+          'userId': userId,
           'roomId': widget.roomId,
           'paymentId': widget.paymentId,
           'checkInDate': widget.checkInDate,
           'checkOutDate': widget.checkOutDate,
-          'status': 'confirmed', // Example field for booking status
-          'createdOn':
-              FieldValue.serverTimestamp(), // Timestamp of booking creation
+          'status': 'confirmed',
+          'createdOn': FieldValue.serverTimestamp(),
         });
 
         print('Booking added and room status updated successfully.');
@@ -74,7 +68,6 @@ class _BookingConfirmationPageState extends State<BookingConfirmationPage> {
 
   @override
   Widget build(BuildContext context) {
-    // Format the check-in and check-out dates
     String formattedCheckInDate =
         DateFormat('dd/MM/yyyy').format(widget.checkInDate);
     String formattedCheckOutDate =
@@ -83,38 +76,74 @@ class _BookingConfirmationPageState extends State<BookingConfirmationPage> {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Booking Confirmation'),
-        centerTitle: true,
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const Text(
-              'Your booking is confirmed!',
-              style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(height: 16),
-            const Text(
-              'Booking Details',
-              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(height: 8),
-            DetailRow(title: 'Room ID:', detail: widget.roomId),
-            DetailRow(title: 'Payment ID:', detail: widget.paymentId),
-            DetailRow(title: 'Check-In Date:', detail: formattedCheckInDate),
-            DetailRow(title: 'Check-Out Date:', detail: formattedCheckOutDate),
-            const SizedBox(height: 32),
-            Center(
-              child: ElevatedButton(
-                onPressed: () {
-                  // Navigate to Home Screen
-                  Navigator.pushReplacementNamed(context, '/home');
-                },
-                child: const Text('Go to Home'),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const SizedBox(height: 30),
+              Center(
+                child: Text(
+                  'Your Booking is Confirmed!',
+                  style: GoogleFonts.poppins(
+                      fontSize: 22, fontWeight: FontWeight.w700),
+                ),
               ),
-            ),
-          ],
+              const SizedBox(height: 30),
+              Card(
+                elevation: 4,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(16),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text(
+                        'Booking Details',
+                        style: TextStyle(
+                            fontSize: 18, fontWeight: FontWeight.bold),
+                      ),
+                      const SizedBox(height: 8),
+                      DetailRow(title: 'Room ID:', detail: widget.roomId),
+                      DetailRow(title: 'Payment ID:', detail: widget.paymentId),
+                      DetailRow(
+                          title: 'Check-In Date:',
+                          detail: formattedCheckInDate),
+                      DetailRow(
+                          title: 'Check-Out Date:',
+                          detail: formattedCheckOutDate),
+                    ],
+                  ),
+                ),
+              ),
+              const SizedBox(height: 32),
+              Center(
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const Bookinghistory()));
+                  },
+                  child: const Text('Go to Booking History'),
+                ),
+              ),
+              const SizedBox(height: 10),
+              Center(
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.pushReplacementNamed(context, '/home');
+                  },
+                  child: const Text('Go to Home'),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
